04599c741978942fc3552283110b34ba5f737364
Enrique P Mosqueda <epesqueda-mosqueda@csumb.edu>
2019-02-27 19:47:10 +0000

    added css

--- Labs/Ajax/index.php
+++ Labs/Ajax/index.php
@@ -16,30 +16,120 @@
     text-align: center;
   }
   form{
-    padding-left:15%;
-    padding-right:15%;
-    
+    padding-left:35%;
+    padding-right:35%;
   }
+  #zipOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #cityOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #latOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #longOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #firstNameOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #lastNameOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #emailOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #phoneNumberOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #zipOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #cityOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #latOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #longOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #stateOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #countyOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #validUser:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #passwordOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #validPasswordOut:hover{
+    color:black;
+    text-decoration:underline;
+    text-decoration-color:teal;
+  }
+  #check{
+    width:90px;
+    height:50px;
+    border-radius: 10px;
+  }
+  
 </style>
 <body id="dummybodyid">
   <h1> Sign Up Form </h1>
   <form>
     <fieldset>
       <legend>Sign Up</legend>
-      <div><label>First Name:</label><input id = "firstName" type="text"></div>
-      <div><label>Last Name:</label><input id = "lastName" type="text"></div>
-      <div><label>Email:</label><input id="email" type="text"></div>
-      <div><label>Phone Number:</label><input id= "phoneNumber" type="text"></div>
-      <div id= "zipOut"><label>Zip Code:</label><input id= "ZP" type="text" value= ""><div id="invalidZip"></div></div>
-      <div id= "cityOut"><label>City:</label><div id= "showCity"></div></div>
-      <div id= "latOut"><label>Latitude:</label> <div id= "lat"></div></div>
-      <div id="longOut"><label>Longitude:</label><div id= "long"></div></div>
-      <div><label>State:</label><input id = "showState" type="text" value =""></div>
-      <div><label>Select a County:</label><select id = "counties"><option id='displayCounty' value=''></option></select></div>
+      <div id = "firstNameOut"><label>First Name:</label><input id = "firstName" type="text"></div>
+      <div id = "lastNameOut"><label>Last Name:</label><input id = "lastName" type="text"></div>
+      <div id = "emailOut"><label>Email:</label><input id="email" type="text"></div>
+      <div id = "phoneNumberOut"><label>Phone Number:</label><input id= "phoneNumber" type="text"></div>
+      <div id = "zipOut"><label>Zip Code:</label><input id= "ZP" type="text" value= ""><div id="invalidZip"></div></div>
+      <div id = "cityOut"><label>City:</label><div id= "showCity"></div></div>
+      <div id = "latOut"><label>Latitude:</label> <div id= "lat"></div></div>
+      <div id = "longOut"><label>Longitude:</label><div id= "long"></div></div>
+      <div id = "stateOut"><label>State:</label><input id = "showState" type="text" value =""></div>
+      <div id = "countyOut"><label>Select a County:</label><select id = "counties"><option id='displayCounty' value=''></option></select></div>
       <div id = "validUser"><label>Desired Username:</label><input id= "userName"type="text"><div id="invalid"></div></div>
-      <div><label>Password:</label><input id="userPassword" type="password"></div>
-      <div id= "validPassword"><label>Type Password Again:</label><input id = "userPassword2" type="password"><div id="invalidPassword"></div></div>
-      <div><input id = "buttonSubmittion" type="button" value="Sign up!"></div>
+      <div id = "passwordOut"><label>Password:</label><input id="userPassword" type="password"></div>
+      <div id= "validPasswordOut"><label>Type Password Again:</label><input id = "userPassword2" type="password"><div id="invalidPassword"></div></div>
+      <div><input id = "check" type="button" value="Sign up!"></div>
     </fieldset>
   </form>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
@@ -49,7 +139,7 @@
     $("#ZP").on("input",getZP);
     $("#showState").on("input", getCounties);
     
-    $("#buttonSubmittion").on("click", function(){
+    $("#check").on("click", function(){
       $("#invalid").html("")
       $("#invalidPassword").html("")
       var userNameInfo = document.getElementById('userName');
@@ -66,19 +156,19 @@
         for(var i = 0; i< userNames.length; i++){
           if(userNames[i] == inputtext.value){
             if(inputtext.value == ""){
-              $("#invalid").html("No username input!");
+              $("#invalid").html("No username input!").css("color","red");
               return;
             }
-            $("#invalid").html("Username already exists!");
+            $("#invalid").html("Username already exists!").css("color","red");
             return;
           }
         }
         if(passwordOne != passwordTwo){
-          $("#invalidPassword").html("Does not mach Password Entered above.");
+          $("#invalidPassword").html("Does not mach Password Entered above.").css("color","red");
           return;
         }
         if(passwordOne =="" && passwordTwo==""){
-          $("#invalidPassword").html("No Password input");
+          $("#invalidPassword").html("No Password input").css("color","red");
           return;
         }
         userNames.splice(1,0,inputtext.value);
@@ -99,7 +189,7 @@
         document.getElementById("userPassword2").value = "";
       }
       else {
-        $("#invalid").html("Username already exists!");
+        $("#invalid").html("Username already exists!").css("color","red");
       }
     }
     function getZP(){
@@ -113,7 +203,7 @@
           
           success: function(data, status){
             if(data === false){
-              $("#invalidZip").html("Zip code not found");
+              $("#invalidZip").html("Zip code not found").css("color","red");
             }
             else{
             $("#invalidZip").html("");
@@ -137,12 +227,12 @@
           },
           success: function(data, status){
             $('#counties option:not(:first)').remove();
-            if(data ===false){
-              $("#display").val("State not found");
+            if(data === false){
+              $("#display").val("State not found").css("color","red");
             }
             else{
-              for(var i = 0; i<data.length; i++){
-                $("#counties").append("<option>"+ data[i]['county']+"</option>");
+              for(var i = 0; i < data.length; i++){
+                $("#counties").append("<option>" + data[i]['county'] + "</option>");
               }
             }
           }
