7592583564eba27b516d732cf4ebe9371028965a
Enrique P Mosqueda <epesqueda-mosqueda@csumb.edu>
2019-03-11 20:23:15 +0000

    lab7

---
+++ Practices/p6/api/getRandomProduct.php
@@ -0,0 +1,37 @@
+<?php
+
+  
+        
+        $products = array();
+        
+        $product = array();
+        $product["product"] = "microfiber beach towel";
+        $product["price"] = 40;
+        $product["qty"] = 2;
+        
+        array_push($products,$product);
+        
+        $product["product"] = "flip flop sandals";
+        $product["price"] = 30;
+        $product["qty"] = 5;
+        
+        array_push($products,$product);
+        
+        $product["product"] = "sun screen 80SPF";
+        $product["price"] = 25;
+        $product["qty"] = 3;
+        
+        array_push($products,$product);
+        
+        $product["product"] = "plastic flying disk";
+        $product["price"] = 15;
+        $product["qty"] = 4;
+        
+        array_push($products,$product);
+        
+        $product["product"] = "beach umbrella";
+        $product["price"] = 75;
+        $product["qty"] = 1;
+        echo json_encode($products[rand(0,5)]);
+
+?>
